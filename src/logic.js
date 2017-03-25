055782cfd42431f2002febdc0fdd42a83d89a44f
Raagun <modestas.peciokas@gmail.com>
2017-03-25 10:35:20 +0200

    basic move logic

--- src/logic.js
+++ src/logic.js
@@ -1,9 +1,56 @@
-function gameLogic(gameJson) {
+function gameLogic(input) {
 
-	console.log("logic module got json: ", gameJson);
+    var map = convert(input.Map);
+    return doMoveForPlayer(map, input.EnPlayerState[input.YourIndex]);
 
-	return {"wrap": gameJson};
+}
+function convert(map) {
+    return map.Rows.map(function (row) {
+        return row.split("");
+    });
+}
 
+function doMoveForPlayer(map, playerData) {
+    return {
+        Moves: playerData.RatPositions.map(function (ratPos) {
+            return createRateMove(map, ratPos);
+        })
+    };
+}
+
+function createRateMove(map, ratInfo) {
+    var move = getNextMove(map, ratInfo.Position);
+    move.RatId = ratInfo.RatId;
+    return move;
+}
+
+function getNextMove(map, position) {
+    var row = position.Row;
+    var col = position.Col;
+    var moves = [
+        getElement(row, col + 1, map),
+        getElement(row, col - 1, map),
+        getElement(row + 1, col, map),
+        getElement(row - 1, col, map)
+    ];
+    var validMove = moves.find(function (move) {
+        return move.element !== "#";
+    });
+    return getMoveAction(validMove.row, validMove.col);
+}
+
+function getMoveAction(row, col) {
+    return {
+        Action: "Move",
+        Position: {
+            Row: row,
+            Col: col
+        }
+    }
+}
+
+function getElement(row, col, map) {
+    return {row: row, col: col, element: map[row][col]};
 }
 
 module.exports = gameLogic;
